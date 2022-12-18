00aed7f69690c3ba4554ed5d94bd9edd3f28c4ea
lesham <leshadavay@gmail.com>
2022-12-18 08:46:49 +0500

    layout fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Layout = ({ children }) => {
   return (
-    <div className="container-fluid vh-100">
-      <div className="column-gap-30 d-flex flex-md-row flex-column justify-content-center align-items-center w-100 h-100">
+    <div className="container-fluid vh-100 d-inline d-md-flex">
+      <div className="column-gap-30 d-flex flex-md-row flex-column justify-content-center align-items-center w-100 h-100 overflow-hidden">
         {children}
       </div>
     </div>
